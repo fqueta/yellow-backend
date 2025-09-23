d9d6a920cb108cecf35802de6358a977bd640fd2
fqueta <ger.maisaqui1@gmail.com>
2025-09-23 12:16:36 -0300

    23/09/2025

--- config/cors.php
+++ config/cors.php
@@ -3,18 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    
-    // Para produção, seja mais específico com as origens
-    'allowed_origins' => env('APP_ENV') === 'production' ? [
-        'https://yellow-dev.maisaqui.com.br',
-        'https://api-yellow-dev.maisaqui.com.br',
-        'http://localhost:3000',
-        'http://localhost:8080',
-    ] : ['*'],
-    
+    'allowed_origins' => ['*'], // Em produção, especifique os domínios
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, // Importante para autenticação
+    'supports_credentials' => true,
 ];
