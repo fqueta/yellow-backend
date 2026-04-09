121d62179c9b66db45ee7237ff5a74895896a1fe
fqueta <ger.maisaqui1@gmail.com>
2026-04-09 18:35:01 -0300

    09/04/2026

--- app/Http/Controllers/api/PointController.php
+++ app/Http/Controllers/api/PointController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Controller para gerenciar pontos dos clientes
