8e59d0ed4ccea6ae36dac640658665bf9ad11231
fqueta <ger.maisaqui1@gmail.com>
2026-04-09 19:13:47 -0300

    09/04/2026

--- app/Http/Controllers/api/PointController.php
+++ app/Http/Controllers/api/PointController.php
@@ -596,8 +596,8 @@ class PointController extends Controller
     {
         $user = $request->user();
         
-        // Permite que administradores assumam a visão de um cliente
-        if ($user && isset($user->permission_id) && isset($this->partner_id) && $user->permission_id < $this->partner_id && $request->filled('admin_client_id')) {
+        // Permite que administradores assumam a visão de um cliente (nível 5 ou menor)
+        if ($user && isset($user->permission_id) && isset($this->partner_id) && $user->permission_id <= $this->partner_id && $request->filled('admin_client_id')) {
             $overriddenUser = \App\Models\User::find($request->admin_client_id);
             if ($overriddenUser) $user = $overriddenUser;
         }
@@ -713,8 +713,8 @@ class PointController extends Controller
     {
         $user = $request->user();
 
-        // Permite que administradores assumam a visão de um cliente
-        if ($user && isset($user->permission_id) && isset($this->partner_id) && $user->permission_id < $this->partner_id && $request->filled('admin_client_id')) {
+        // Permite que administradores assumam a visão de um cliente (nível 5 ou menor)
+        if ($user && isset($user->permission_id) && isset($this->partner_id) && $user->permission_id <= $this->partner_id && $request->filled('admin_client_id')) {
             $overriddenUser = \App\Models\User::find($request->admin_client_id);
             if ($overriddenUser) $user = $overriddenUser;
         }
